07e9d6264200a0beab7b716cb7907ae2b1a6f5c9
Utkarsh123 <usjadhav8495@gmail.com>
2022-09-10 23:42:33 +0530

    This new class added in FirstBranch

---
+++ src/utkarshpro2/Gitclass.java
@@ -0,0 +1,13 @@
+package utkarshpro2;
+
+public class Gitclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hi this is updated git");
+
+	}
+
+}
